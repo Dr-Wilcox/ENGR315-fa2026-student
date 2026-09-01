a7f143c51e552d386440dbab7d3c580f76ad52df
Dr-Wilcox <overthecounterinbox@gmail.com>
2026-09-01 16:06:35 -0400

    Revert "Update formulas.py"
    This reverts commit 3cebc8e9deabbbd8b7b99a8f1a43b1a0986981cc.

--- Module 1 - Language Intro/1 - Variables/0 - Lecture Examples/formulas.py
+++ Module 1 - Language Intro/1 - Variables/0 - Lecture Examples/formulas.py
@@ -1,5 +1,5 @@
 principal = 1000  # initial amount to be deposited
-rate = 2.7  # interest rate applied to deposit (will be divided by 100)
+rate = 1  # interest rate applied to deposit (will be divided by 100)
 n = 10  # number of years to compound deposit
 
 # use an over-loaded form of print() to set out each element of the string
@@ -7,10 +7,9 @@ print("Initial principal is $", principal)
 print("Interest rate is ", rate / 100)
 print("Hold for ", n,  " years")
 
-
-final = principal * ((1 + (rate / 100)) ** n) # calculate the eventual result
+# calculate the eventual result
+final = principal * ((1 + (rate / 100)) ** n)
 
 # fancy print the output with two decimal places for floating number
 print(f"Final value after is ${final:.2f}")
 
-# Here's an example of something extra added but not saved
